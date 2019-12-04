b071fcb813bb99dcc89c0534edfeaba801c59a92
qingjinsunny <qingjin@seas.upenn.edu>
2019-12-04 11:45:41 -0500

    implement pie chart

--- src/worldviewer/chart/PieChartViewer.java
+++ src/worldviewer/chart/PieChartViewer.java
@@ -1,10 +1,26 @@
 package worldviewer.chart;
 
+import javafx.scene.chart.PieChart;
+
 /**
  * Pie Chart Viewer 
  * @author huizhu
  *
  */
-public class PieChartViewer {
+public class PieChartViewer extends ChartViewer {
+	@Override
+	public PieChart newChart() {
+		
+        final PieChart pieChart = new PieChart();
+                
+        pieChart.setTitle("Pie Chart");
+        pieChart.setAnimated(false);
+        return pieChart;
+		
+	}
 
+	@Override
+	public void updateChart(int year) {
+		super.updatePieChart(year);
+	}
 }
